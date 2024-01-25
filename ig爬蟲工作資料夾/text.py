fb32b25381d40818a5b68012900064a0e2aa16f3
hope6301 <65280888+hope6301@users.noreply.github.com>
2024-01-25 22:01:06 +0800

    Update text.py

--- ig爬蟲工作資料夾/text.py
+++ ig爬蟲工作資料夾/text.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost:3306",       # 数据库主机地址
   user="root", # 数据库用户名
-  passwd="hope0208"   # 数据库密码
+  passwd="********常用密碼"   # 数据库密码
 )
 
 mycursor = mydb.cursor()
